fc7a68872df0a4ca3cd6c91400468120379a8bc6
Ccharliiee <27688952+Ccharliiee@users.noreply.github.com>
2021-10-16 20:19:49 -0400

    Replace Hard-code HTML String with JS cost
    1. Replace Hard-code HTML String with JS cost
    2. Use to toDateString() to display a user-friendly date

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,12 +1,15 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 9, 16);
+  const expenseDescription = "Internet Bill";
+  const expenseAmount = 59.95;
   return (
     <div className="expense-item">
-      <div>Oct 13, 2021</div>
+      <div>{expenseDate.toDateString()}</div>
       <div className="expense-item__description">
-        <h2>Internet Bill</h2>
-        <div className="expense-item__price">42.50</div>
+        <h2>{expenseDescription}</h2>
+        <div className="expense-item__price">${expenseAmount} </div>
       </div>
     </div>
   );
