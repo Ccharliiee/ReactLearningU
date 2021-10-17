1db4224be3ff1f01e56b0e929dbf2b4a6077a753
Ccharliiee <27688952+Ccharliiee@users.noreply.github.com>
2021-10-16 20:41:02 -0400

    Move attributes of child component to App component
    1. Added several child component in App component
    2. Moved attributes of child component to App component and pass them as props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import ExpenseItem from "./component/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "id1",
+      title: "Toilet Paper",
+      amount: 46.12,
+      date: new Date(2021, 7, 14),
+    },
+    {
+      id: "id2",
+      title: "New Earphone",
+      amount: 499.49,
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: "id3",
+      title: "Internet Bill",
+      amount: 259.95,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "id4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2020, 5, 12),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem expenseDetail={expenses[0]}></ExpenseItem>
+      <ExpenseItem expenseDetail={expenses[1]}></ExpenseItem>
+      <ExpenseItem expenseDetail={expenses[2]}></ExpenseItem>
+      <ExpenseItem expenseDetail={expenses[3]}></ExpenseItem>
     </div>
   );
 }
